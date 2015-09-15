3ec0b55b75ba84ed45bec1be01930af0c78ba1ba
Nate <npaulemon@gmail.com>
2015-09-15 17:31:15 -0400

    insecure removed

--- client/templates/leaderboard/hs.js
+++ client/templates/leaderboard/hs.js
@@ -5,17 +5,6 @@ Template.leaderboard.helpers({
   }
 });
 
-Meteor.users.deny({
-  insert: function (userId, doc) {
-  	return false;
-  },
-  update: function (userId, doc, fields, modifier) {
-  	return false;
-  },
-  remove: function (userId, doc) {
- 	return false;
-  }
-});
 
 
 
